7d5552a14407d4d591c29cd6fb814f9a24c51b6c
ryan-waltz <45483493+kosciolek@users.noreply.github.com>
2021-07-12 12:17:31 +0200

    asd

--- node/install.sh
+++ node/install.sh
@@ -2,7 +2,7 @@ curl https://raw.githubusercontent.com/kosciolek/env-init/master/node/.eslintrc.
 curl https://raw.githubusercontent.com/kosciolek/env-init/master/.prettierignore > .prettierignore
 curl https://raw.githubusercontent.com/kosciolek/env-init/master/.eslintignore > .prettierignore
 
-yarn add -D husky eslint-config-airbnb-typescript eslint eslint-plugin-import @typescript-eslint/eslint-plugin
+yarn add -D eslint-config-prettier prettier husky eslint-config-airbnb-typescript eslint eslint-plugin-import @typescript-eslint/eslint-plugin
 
 npm set-script prepare "husky install"
 npm set-script lint "yarn prettier . --write && yarn eslint . --fix"

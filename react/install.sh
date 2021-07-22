d7fadfc735859a29a5d6618ce9af175e7c1b0223
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-07-22 16:57:40 +0200

    Update install.sh

--- react/install.sh
+++ react/install.sh
@@ -2,12 +2,12 @@ curl https://raw.githubusercontent.com/kosciolek/env-init/master/react/.eslintrc
 curl https://raw.githubusercontent.com/kosciolek/env-init/master/.prettierignore -o .prettierignore
 curl https://raw.githubusercontent.com/kosciolek/env-init/master/.eslintignore -o .eslintignore
 
-yarn add -D eslint-config-prettier prettier husky eslint-config-airbnb-typescript eslint eslint-plugin-import @typescript-eslint/eslint-plugin eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks
+yarn add -D eslint-config-prettier prettier husky eslint-config-airbnb-typescript eslint eslint-plugin-import @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks
 
 npm set-script prepare "husky install"
 npm set-script lint "eslint ."
-npm set-script lint-fix "prettier . --write && eslint . --fix"
+npm set-script lint:fix "prettier . --write && eslint . --fix"
 npm run prepare
 
-npx husky add .husky/pre-commit "yarn lint-fix"
+npx husky add .husky/pre-commit "yarn lint:fix"
 git add .husky/pre-commit
